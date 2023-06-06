926779bc37fc33c8d83b7be59cc82f9bef82c134
vanyi0924 <vanyi0924@gmail.com>
2023-06-06 18:20:43 +0800

    update: 3005

--- nodejs/publish.mjs
+++ nodejs/publish.mjs
@@ -1,21 +1,22 @@
 import shell from "shelljs";
 
-shell.cd("/Users/vanyi/project/vanyi-blog/Vanyi0924.github.io");
+const TO = "/Users/vanyi/self/Vanyi0924.github.io";
+const FROM = "/Users/vanyi/self/vitepress-blog/docs/.vitepress/dist/*";
+
+shell.cd(TO);
 
 shell.ls().forEach((filename) => {
   shell.rm("-r", filename);
-})
+});
 
+shell.cp("-R", FROM, ".");
 
-shell.cp(
-  "-R",
-  "/Users/vanyi/project/vanyi-blog/vitepress-blog/docs/.vitepress/dist/*",
-  "."
-);
-
-const now = new Date()
-const time = now.toLocaleDateString().replaceAll("/", "-") + " " + now.toLocaleTimeString()
-shell.exec(`git pull`)
-shell.exec(`git add .`)
-shell.exec(`git commit -am "auto update:${time}"`)
-shell.exec(`git push`)
+const now = new Date();
+const time =
+  now.toLocaleDateString().replaceAll("/", "-") +
+  " " +
+  now.toLocaleTimeString();
+shell.exec(`git pull`);
+shell.exec(`git add .`);
+shell.exec(`git commit -am "auto update:${time}"`);
+shell.exec(`git push`);
